426f0bc271bbfe6cc20d22398d67fdd475d2f600
Sergey Blednov <sergey.blednov@pixacore.com>
2017-04-18 15:09:40 -0400

    Added assets. Setup floor, pins and a ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public float launchSpeed;
+
+	private AudioSource audioSource;
+	private Rigidbody rigidBody;
+
+	void Start () 
+	{
+		audioSource = GetComponent<AudioSource> ();
+		rigidBody = GetComponent<Rigidbody> ();
+		Launch();
+	}
+
+	public void Launch ()
+	{
+		rigidBody.velocity  = new Vector3 (0, 0, launchSpeed);
+		audioSource.Play ();
+	}
+}
