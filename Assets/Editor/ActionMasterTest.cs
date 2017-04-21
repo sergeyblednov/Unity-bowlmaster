4005ad32546e2b96f9e9b0ab0ef7f80de7df82ea
Sergey Blednov <sergey.blednov@pixacore.com>
2017-04-21 12:45:51 -0400

    Started with unity testing

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+[TestFixture]
+public class ActionMasterTest {
+
+	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+//	private ActionMaster.Action reset = ActionMaster.Action.Reset;
+
+	[Test]
+	public void PassingTest ()
+	{
+		Assert.AreEqual (1, 1);
+	}
+
+	[Test]
+	public void T01OneStrikeReturnsEndTurn ()
+	{
+		ActionMaster actionMaster = new ActionMaster ();
+		Assert.AreEqual (endTurn, actionMaster.Bowl (10));
+	}
+
+//	[Test]
+//	public void FirstStrikeReturnReset ()
+//	{
+//		ActionMaster actionMaster = new ActionMaster ();
+//		Assert.AreEqual (reset, actionMaster.Bowl (10));
+//	}
+}
---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster {
+
+	public enum Action { Tidy, Reset, EndTurn, EndGame };
+
+	public Action Bowl(int pins) 
+	{
+		//Other behaviour here 
+
+		if (pins == 10) {
+			return Action.EndTurn;
+		}
+
+		//Other behaviour here 
+
+		throw new UnityException ("Not sure what action to return!");
+	}
+}
